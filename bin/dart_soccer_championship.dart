ad1a8c53f6060b992eb8312bfc0ab1f3fa9b48b5
João Pedro <jpedrodaibello1999@outlook.com>
2022-06-01 23:29:16 -0300

    feat: create Dart project

---
+++ bin/dart_soccer_championship.dart
@@ -0,0 +1,5 @@
+import 'package:dart_soccer_championship/dart_soccer_championship.dart' as dart_soccer_championship;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_soccer_championship.calculate()}!');
+}
---
+++ test/dart_soccer_championship_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_soccer_championship/dart_soccer_championship.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
