42211359f43f2c3069817252895b90dba9bdfdb2
João Pedro <jpedrodaibello1999@outlook.com>
2022-06-05 22:49:21 -0300

    feat: configure Appwrite

--- lib/app/config/application_config.dart
+++ lib/app/config/application_config.dart
@@ -1,7 +1,31 @@
+import 'package:dart_appwrite/dart_appwrite.dart';
+import 'package:dart_soccer_championship/app/config/appwrite_client_configuration.dart';
 import 'package:dotenv/dotenv.dart';
+import 'package:get_it/get_it.dart';
 
 class ApplicationConfig {
+  Client? appwriteClient;
+
   Future<void> loadApplicationConfig() async {
+    await _loadAppwriteConfig();
+  }
+
+  Future<void> _loadAppwriteConfig() async {
     final env = DotEnv(includePlatformEnvironment: true)..load();
+
+    final appwriteConfig = AppwriteClientConfiguration(
+      endpoint: env['APPWRITE_API_ENDPOINT']!,
+      projectId: env['APPWRITE_PROJECT_ID']!,
+      apiKeySecret: env['APPWRITE_API_KEY_SECRET']!,
+    );
+
+    appwriteClient = Client();
+
+    appwriteClient!
+        .setEndpoint(appwriteConfig.endpoint)
+        .setProject(appwriteConfig.projectId)
+        .setKey(appwriteConfig.apiKeySecret);
+
+    GetIt.I.registerSingleton(appwriteConfig);
   }
 }
---
+++ lib/app/config/appwrite_client_configuration.dart
@@ -0,0 +1,11 @@
+class AppwriteClientConfiguration {
+  final String endpoint;
+  final String projectId;
+  final String apiKeySecret;
+
+  AppwriteClientConfiguration({
+    required this.endpoint,
+    required this.projectId,
+    required this.apiKeySecret,
+  });
+}
