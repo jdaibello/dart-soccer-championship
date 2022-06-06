c7aa52ae9041567e1a12a88aaa008e632ea99b08
João Pedro <jpedrodaibello1999@outlook.com>
2022-06-05 21:06:39 -0300

    refactor: clear project

--- bin/dart_soccer_championship.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_soccer_championship/dart_soccer_championship.dart' as dart_soccer_championship;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_soccer_championship.calculate()}!');
-}
--- test/dart_soccer_championship_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_soccer_championship/dart_soccer_championship.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
